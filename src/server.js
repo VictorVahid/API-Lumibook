a2230f81f32e81e0952097fe53a118b8b7b8c0a2
Victor Vahid <victorvahid76@gmail.com>
2025-05-14 15:45:12 -0400

    refactor: standardize import paths and formatting in NotificationController and SendNotification

--- src/server.js
+++ src/server.js
@@ -1,14 +1,13 @@
-// src/server.js
+ // src/server.js
 require("dotenv").config();
 const mongoose = require("mongoose");
+mongoose.set("strictQuery", false); // opcional: suprime warnings futuros de strictQuery
 const app = require("./app");
 
 const PORT = process.env.PORT || 3000;
+
 mongoose
-	.connect(process.env.MONGO_URI, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	})
+	.connect(process.env.MONGO_URI) // sem flags depreceadas
 	.then(() => {
 		console.log("MongoDB conectado");
 		app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
@@ -17,3 +16,4 @@ mongoose
 		console.error("Erro ao conectar no MongoDB:", err);
 		process.exit(1);
 	});
+
--- src/usecases/sendNotification.js
+++ src/usecases/sendNotification.js
@@ -1,23 +1,22 @@
-const EmailService = require('../../../infrastructure/services/EmailService');
-const Notification = require('../../../domain/models/Notification');
-
+const EmailService = require("../../src/infrastructure/services/EmailService");
+const Notification = require("../../src/domain/models/Notification");
 class SendNotification {
-  async execute(type, recipient, data) {
-    const notification = new Notification({
-      type,
-      recipient,
-      content: data
-    });
+	async execute(type, recipient, data) {
+		const notification = new Notification({
+			type,
+			recipient,
+			content: data,
+		});
 
-    try {
-      await EmailService.send(type, recipient, data);
-      notification.markAsSent();
-      return notification;
-    } catch (error) {
-      notification.markAsFailed();
-      throw error;
-    }
-  }
+		try {
+			await EmailService.send(type, recipient, data);
+			notification.markAsSent();
+			return notification;
+		} catch (error) {
+			notification.markAsFailed();
+			throw error;
+		}
+	}
 }
 
 module.exports = SendNotification;
